6090f537380818fdebacbdc193f85afb7de4ea5b
EmTee82 <204011885+EmTee82@users.noreply.github.com>
2025-03-27 00:19:29 +1100

    initialise site build

---
+++ Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav style={{ padding: '1rem', backgroundColor: '#222', color: '#fff' }}>
+      <h1>FansifAI</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
